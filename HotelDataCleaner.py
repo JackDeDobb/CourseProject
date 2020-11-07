75dd3371d7997452edf6f8ea813146506a9d4270
Jackson DeDobbelaere <jackson.dedobbelaere@c3.ai>
2020-11-07 00:14:45 -0800

    adding second pass to filter for words across collection

--- HotelDataCleaner.py
+++ HotelDataCleaner.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import json
 import os
 import string
@@ -14,24 +15,25 @@ with open('/'.join([currDirectoryOfScript, 'StopWords.json'])) as stopWords:
 hotelIDToDataMapping = {}
 for hotelDataFileName in hotelDataFileNames:
   with open('/'.join([hotelDataFilesDirectory, hotelDataFileName])) as hotelDataFile:
+    print('loading in file: ' + hotelDataFileName)
     hotelData = json.load(hotelDataFile)
     hotelID = hotelData['HotelInfo']['HotelID']
     hotelIDToDataMapping[hotelID] = hotelData
 
 
-reviewCounter = 0
 # Part 1:   Remove reviews with any missing aspect rating or document
 #           length less than 50 words
 # Part 2:   Convert all the words into lower cases
 # Part 3.1: Remove punctuation and stop words
+# Part 3.2: Gather -- Remove words occuring in less than 10 reviews in the collection
 ratingCategories = ['Service', 'Cleanliness', 'Overall', 'Value', 'Sleep Quality', 'Rooms', 'Location']
+wordToReviewOccurencesMapping = defaultdict(int)
 for (hotelID, hotelData) in list(hotelIDToDataMapping.items()):
-  hotelReviews = hotelData['Reviews']
+  print('first pass: ' + hotelID)
   filteredHotelReviews = []
-  for hotelReview in hotelReviews:
+  for hotelReview in hotelData['Reviews']:
     if 'Content' not in hotelReview or len(hotelReview['Content'].split()) < 50:
       continue
-
     try:
       [float(hotelReview['Ratings'][ratingCategory]) for ratingCategory in ratingCategories]
     except:
@@ -39,14 +41,27 @@ for (hotelID, hotelData) in list(hotelIDToDataMapping.items()):
 
     hotelReview['Content'] = hotelReview['Content'].lower()
     hotelReview['Content'] = hotelReview['Content'].translate(str.maketrans('', '', string.punctuation))
-    hotelReview['Content'] = ' '.join(list(filter(lambda x: x not in stopWords, hotelReview['Content'].split())))
+    hotelReview['Content'] = list(filter(lambda x: x not in stopWords, hotelReview['Content'].split()))
+    filteredHotelReviews.append(hotelReview)
+
+    for word in set(hotelReview['Content']):
+      wordToReviewOccurencesMapping[word] += 1
+
+  hotelIDToDataMapping[hotelID]['Reviews'] = filteredHotelReviews
+  if len(filteredHotelReviews) == 0:
+    del hotelIDToDataMapping[hotelID]
+
+
+# Part 3.2: Apply -- Remove words occuring in less than 10 reviews in the collection ()
+wordToReviewOccurencesMapping = { k: v for k, v in wordToReviewOccurencesMapping.items() if v >= 10 }
+wordSetToKeep = set(wordToReviewOccurencesMapping.keys())
+for (hotelID, hotelData) in list(hotelIDToDataMapping.items()):
+  print('second pass: ' + hotelID)
+  filteredHotelReviews = []
+  for hotelReview in hotelData['Reviews']:
+    hotelReview['Content'] = ' '.join(list(filter(lambda x: x in wordSetToKeep, hotelReview['Content'])))
     filteredHotelReviews.append(hotelReview)
 
   hotelIDToDataMapping[hotelID]['Reviews'] = filteredHotelReviews
   if len(filteredHotelReviews) == 0:
     del hotelIDToDataMapping[hotelID]
-  reviewCounter += len(filteredHotelReviews)
-
-print('hotelCounter: ' + str(len(hotelIDToDataMapping)))
-print('reviewCounter: ' + str(reviewCounter))
-#print(hotelIDToDataMapping)
