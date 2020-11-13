ddbb77027519a117a36c2ca067dec91d171a11b3
UIUC <uiuc@Subramanian-Shankar-2.local>
2020-11-13 06:46:10 -0800

    Added ratings analysis

--- main.py
+++ main.py
@@ -11,12 +11,22 @@ try:
   _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
   pass
-
-# TODO: should be if somewhere
+#The try block does not raise any errors, so the else block is executed
 else:
      ssl._create_default_https_context = _create_unverified_https_context
 nltk.download('punkt')
 
+def parseWordsForSentence(content, vocab, vocabDict):
+  # Use nltk and stopwords to tokenize words for  each review
+  tokenizedWords = []
+  sentences = nltk.sent_tokenize(content)
+  for sent in sentences:
+    words = nltk.word_tokenize(sent)
+    stemmedWordsInVocab = [stemmer.stem(w.lower()) for w in words if stemmer.stem(w.lower()) in vocabDict]
+    words = [vocabDict.get(w) for w in stemmedWordsInVocab]
+    if len(words) > 0:
+      tokenizedWords.append(words)
+  return tokenizedWords
 
 
 def parseWords(content, stopWords):
@@ -36,16 +46,12 @@ def genStopwords():
   with open('/'.join([currDirectoryOfScript, 'StopWords.json'])) as stopWords:
     return set(json.load(stopWords))
 
-def readData():
-  # Load and read all json files
-  pass
-
-def createVocab(stopWords):
+def createVocab(reviewDataList,stopWords):
   # Iterate through all the json files data and create vocabulary dictionary having the words and their associated counts
   # Use parseWords to generate the tokenized terms
   # Use nltk.FreqDist to generate term frequqnecies
   allTerms = []
-  for reviewData in reviewDataLists:
+  for reviewData in reviewDataList:
     for review in reviewData['Reviews']:
       parseWordsInReview = []
       for parseWord in parseWords(review['Content'], stopWords):
@@ -64,43 +70,73 @@ def createVocab(stopWords):
   vocabDict = dict(zip(vocab, range(len(vocab))))
   return vocab, cnt, vocabDict
 
-def saveFile(path):
-  # Save to file
-  pass
-
-def loadFile(path):
-  # Load from file
-  pass
 
 def generateAspectTerms(aspectLines,vocabDict):
-  # Go through each line of the fule and use vocabdict to get the tokenized word
+  # TODO: Aspect modeling
   pass
 
-def addAspectWords(analyzer, p, NumIter,c):
-  # Update aspect Words based on Expectation step of EM algorithm
+def addAspectWords():
+  # TODO: Update aspect Words based on Expectation step of EM algorithm
   pass
+
+def calculateSForReview(reviewWords,aspectTerms,vocab):
+    #Calculate s for each review and aspect
+    s_aspect_word = np.zeros(len(aspectTerms),len(reviewWords))
+    for aspect in range(len(aspectTerms)):
+        for word in reviewWords:
+            i = vocab.index(word) # Get the index
+            s_aspect_word[aspect,i] = num_aspect_word[aspect,i]+1
+    return s_aspect_word
+
+def createWMatrixForEachReview(review,vocab, vocabDict,aspectTerms):
+    #Genrate the matrix for each review
+    reviewWords=parseWordsForSentence(review, vocab, vocabDict)
+    reviewMatrix = np.zeros((len(aspectTerms),len(reviewWords)))
+    s_aspect_word=calculateSForReview(reviewWords,aspectTerms,vocab)
+    for aspect in range(len(aspectTerms)):
+        for word in range(len(reviewWords)):
+            sum_row = sum(s_aspect_word[aspect])
+            if  sum_row > 0:
+                reviewMatrix = s_aspect_word[aspect,word]/sum_row
+    return reviewWords, reviewMatrix
+
+def createWordMatrix(reviewDataList,vocab, vocabDict,aspectTerms):
+   # Ratings analysis and generate review matrix list
+   reviewWordsList=[]
+   for reviews in reviewDataList:
+       for review in reviews:
+           reviewWords, reviewMatrix=createWMatrixForEachReview(review,vocab, vocabDict,aspectTerms)
+           reviewWordsList.append(reviewWords)
+           reviewMatrixList.append(reviewMatrix)
+   return  reviewWordsList,reviewMatrixList
+
+def generateResults(reviewDataList,reviewWordsList,reviewMatrixList, finalFile):
+    f = open(finalFile,"w")
+    for reviews in reviewDataList:
+        for review in reviews:
+            f.write(review["ReviewID"]+":"review+":"+str(reviewWordsList[review])+":"+str(reviewMatrixList[review])+"\n")
 
 def getVocab():
   ##### Step 1: Create vocabulary from json files
   stopWords = genStopwords()
   reviewDataList = getData('HotelData/CleanData') ##Read the json files
-  return createVocab(reviewDataList,stopWords)
+  vocab, cnt, vocabDict=createVocab(reviewDataList,stopWords)
+  return reviewDataList,vocab, cnt, vocabDict
 
 
 def runBootstrap():
-  ##### step 2. Run bootstrapping method on vocab
+  ##### step 2. Run model on vocab
   # Loading vocab data from saved file
-  vocab, cnt, vocabDict = getVocab()
+  reviewDataList, vocab, cnt, vocabDict = getVocab()
 
-  # Load aspect words
-  aspectLines = loadFile(aspectWordsFilePath)
-  # Get the stemmed term for each aspect from the vocabDict
+  # Get the stemmed term for each aspect from aspect modeling
   aspectTerms = generateAspectTerms(aspectLines, vocabDict)
 
   #### Run EM algorithm on aspect keywords and save it to a file
   addAspectWords(vocabDict)
 
   # Create the word matrix for all the reviews
-  createWordMatrix()
+  reviewWordsList,reviewMatrixList=createWordMatrix(reviewDataList,vocab, vocabDict,aspectTerms)
+  finalFile="finalresults.txt"
   # Use the word matrix to generate the results
-  generateResults()
+  generateResults(reviewDataList,reviewWordsList,reviewMatrixList, finalFile)
