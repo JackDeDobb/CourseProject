88bc60c7262a9558c07b0c620b60761fdcc817c9
Jackson DeDobbelaere <jackson.dedobbelaere@c3.ai>
2020-11-23 20:59:14 -0600

    product analysis file done

---
+++ ProductAnalysis.py
@@ -0,0 +1,47 @@
+# Use Python3
+from AnalysisMethods import *
+from scipy.special import digamma, gammaln
+import json
+import nltk
+import numpy as np
+import os
+import ssl
+
+
+# Dependencies Below
+#######################################################################
+nltk.download('punkt')
+stemmer = nltk.stem.porter.PorterStemmer()
+try:
+  _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+  pass
+else:
+  ssl._create_default_https_context = _create_unverified_https_context
+#######################################################################
+
+
+def createVocab(reviewDataList, stopWords):
+  # Fill in method here
+  pass
+
+
+def generateResults(productList, reviewDataList, reviewLabelList, reviewWordsList, reviewMatrixList, finalFile):
+  # Fill in method here
+  pass
+
+
+def getData(folder):
+  # Fill in method here
+  pass
+
+
+def getVocab():
+  # Fill in method here
+  pass
+
+
+if __name__ == '__main__':
+  productList, reviewDataList, vocab, cnt, vocabDict, reviewList = getVocab() # Loading vocab data from saved file
+  reviewLabelList, reviewWordsList, reviewMatrixList = runAlgorithm(vocab, cnt, vocabDict, reviewList)
+  generateResults(productList, reviewDataList, reviewLabelList, reviewWordsList, reviewMatrixList, 'ProductFinalResults.txt') # Use the word matrix to generate the results
