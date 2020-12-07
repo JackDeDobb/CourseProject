242dee9eadbc3e98b2fbc1200638e62d00ed4291
Jackson DeDobbelaere <jackson.dedobbelaere@c3.ai>
2020-12-06 20:07:16 -0600

    finally got it

--- Analysis/HotelAnalysis.py
+++ Analysis/HotelAnalysis.py
@@ -65,47 +65,6 @@ def createVocab(reviewDataList, itemList, stopWords):
   return vocab, cnt, vocabDict, reviewList, reviewFreqDictList, itemIdList, reviewIdList, reviewContentList, reviewRatingList, reviewAuthorList, allReviewsList
 
 
-def createWMatrixForEachReview(reviewWordsDict, reviewLabels): # Generate the matrix for each review
-  review = list(reviewWordsDict.keys())
-  reviewMatrix = np.zeros((len(reviewLabels), len(review)))
-  for i in range(len(reviewLabels)):
-    for j in range(len(review)):
-      reviewMatrix[i, j] = reviewWordsDict[review[j]] * reviewLabels[i][j] # Get the review rating
-    reviewMatrix[i] = (reviewMatrix[i] - reviewMatrix[i].min(0)) / reviewMatrix[i].ptp(0) # Normalizing without negative values
-  return reviewMatrix
-
-
-def createWordMatrix(reviewFreqDictList, reviewLabelList): # Ratings analysis and generate review matrix list
-  reviewMatrixList = []
-  for i in range(len(reviewFreqDictList)):
-    reviewMatrixList.append(createWMatrixForEachReview(reviewFreqDictList[i], reviewLabelList[i]))
-  return reviewMatrixList
-
-
-def generatePredictedAspects(reviewFreqDictList, reviewMatrixList):
-  predList = []
-  for i in range(len(reviewMatrixList)):
-    for j in range(len(reviewMatrixList[i])):
-      predReviews = 0
-      for k in range(len(reviewMatrixList[i][j])):
-        review = list(reviewFreqDictList[i].keys())
-        predReviews += reviewFreqDictList[i][review[k]]*reviewMatrixList[i][j][k]
-      predReviews = predReviews/len(reviewMatrixList[i][j])
-      predList.append(predReviews)
-  predList = [float(i) * 5 / max(predList) for i in predList]
-  return predList
-
-
-def runAlgorithm(vocabDict, reviewFreqDictList, allReviewsList):
-  mu, sigma = generateAspectParameters(reviewFreqDictList, vocabDict) # Aspect modeling to get parameters
-  reviewLabelList = sentenceLabeling(mu, sigma, reviewFreqDictList, 7) # Create aspects and get labels from aspect terms on reviews
-  reviewMatrixList = createWordMatrix(reviewFreqDictList, reviewLabelList) # Create the word matrix for all the reviews
-  positiveWordList, negativeWordList = getOverallRatingsForWords(reviewFreqDictList, reviewMatrixList)
-  predList = generatePredictedAspects(reviewFreqDictList, reviewMatrixList)
-  totalMse, totalPearson = getStats(predList, allReviewsList)
-  return reviewLabelList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson
-
-
 if __name__ == '__main__':
   currDirectoryOfScript = os.path.dirname(os.path.realpath(__file__))
   cleanDataLocation = '/'.join([currDirectoryOfScript, '..', 'Data', 'HotelData', 'testData']) # TODO: switch from testData to CleanData
@@ -115,6 +74,6 @@ if __name__ == '__main__':
   print('DEBUG: getData')
   vocab, cnt, vocabDict, reviewList, reviewFreqDictList, itemIdList, reviewIdList, reviewContentList, reviewRatingList, reviewAuthorList, allReviewsList = createVocab(reviewDataList, itemList, stopWords)
   print('DEBUG: createVocab')
-  reviewLabelList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson = runAlgorithm(vocabDict, reviewFreqDictList, allReviewsList)
+  reviewLabelList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson = runAlgorithm(vocabDict, reviewFreqDictList, allReviewsList, 7)
   print('DEBUG: run algo')
   generateResults(itemIdList, reviewIdList, reviewContentList, reviewRatingList, reviewAuthorList, reviewDataList, reviewLabelList, reviewList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson, resultsLocation) # Use the word matrix to generate the results
--- Analysis/ProductAnalysis.py
+++ Analysis/ProductAnalysis.py
@@ -59,45 +59,6 @@ def createVocab(reviewDataList, itemList, stopWords):
   return vocab, cnt, vocabDict, reviewList, reviewFreqDictList, itemIdList, reviewIdList, reviewContentList, reviewRatingList, reviewAuthorList, allReviewsList
 
 
-def createWMatrixForEachReview(reviewWordsDict, reviewLabels): # Generate the matrix for each review
-  review = list(reviewWordsDict.keys())
-  reviewMatrix = np.zeros(len(review))
-  for j in range(len(review)):
-    reviewMatrix[j] = reviewWordsDict[review[j]] * reviewLabels[0][j] # Get the review rating
-  reviewMatrix = (reviewMatrix - reviewMatrix.min(0)) / reviewMatrix.ptp(0)
-  return reviewMatrix
-
-
-def createWordMatrix(reviewFreqDictList, reviewLabelList): # Ratings analysis and generate review matrix list
-  reviewMatrixList = []
-  for i in range(len(reviewFreqDictList)):
-    reviewMatrixList.append(createWMatrixForEachReview(reviewFreqDictList[i], reviewLabelList[i]))
-  return reviewMatrixList
-
-
-def generatePredictedAspects(reviewFreqDictList, reviewMatrixList):
-  predList = []
-  for j in range(len(reviewMatrixList)):
-    predReviews = 0
-    for k in range(len(reviewMatrixList[j])):
-      review = list(reviewFreqDictList[j].keys())
-      predReviews += reviewFreqDictList[j][review[k]]*reviewMatrixList[j][k]
-    predReviews = predReviews/len(reviewMatrixList[j])
-    predList.append(predReviews)
-  predList = [float(i) * 5 / max(predList) for i in predList]
-  return predList
-
-
-def runAlgorithm(vocabDict, reviewFreqDictList, allReviewsList):
-  mu, sigma = generateAspectParameters(reviewFreqDictList, vocabDict) # Aspect modeling to get parameters
-  reviewLabelList = sentenceLabeling(mu, sigma, reviewFreqDictList, 1) # Create aspects and get labels from aspect terms on reviews
-  reviewMatrixList = createWordMatrix(reviewFreqDictList, reviewLabelList) # Create the word matrix for all the reviews
-  positiveWordList, negativeWordList = getOverallRatingsForWords(reviewFreqDictList, reviewMatrixList)
-  predList = generatePredictedAspects(reviewFreqDictList, reviewMatrixList)
-  totalMse, totalPearson = getStats(predList, allReviewsList)
-  return reviewLabelList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson
-
-
 if __name__ == '__main__':
   currDirectoryOfScript = os.path.dirname(os.path.realpath(__file__))
   cleanDataLocation = '/'.join([currDirectoryOfScript, '..', 'Data', 'ProductData', 'testData']) # TODO: switch from testData to CleanData
@@ -107,6 +68,6 @@ if __name__ == '__main__':
   print('DEBUG: getData')
   vocab, cnt, vocabDict, reviewList, reviewFreqDictList, itemIdList, reviewIdList, reviewContentList, reviewRatingList, reviewAuthorList, allReviewsList = createVocab(reviewDataList, itemList, stopWords)
   print('DEBUG: createVocab')
-  reviewLabelList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson = runAlgorithm(vocabDict, reviewFreqDictList, allReviewsList)
+  reviewLabelList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson = runAlgorithm(vocabDict, reviewFreqDictList, allReviewsList, 1)
   print('DEBUG: run algo')
   generateResults(itemIdList, reviewIdList, reviewContentList, reviewRatingList, reviewAuthorList, reviewDataList, reviewLabelList, reviewList, reviewMatrixList, positiveWordList, negativeWordList, totalMse, totalPearson, resultsLocation) # Use the word matrix to generate the results
