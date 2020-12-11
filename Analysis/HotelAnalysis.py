62e0aa2dad502dd12a694a5e89b3891e09129626
Jackson DeDobbelaere <jackdedobb@me.com>
2020-12-10 21:39:18 -0600

    parameterize results stuff (#15)
    * stuff
    
    * slight

--- Analysis/HotelAnalysis.py
+++ Analysis/HotelAnalysis.py
@@ -4,6 +4,7 @@ import nltk
 import numpy as np
 import os
 import ssl
+import sys
 
 
 # Dependencies Below
@@ -66,9 +67,10 @@ def createVocab(reviewDataList, itemList, stopWords):
 
 
 if __name__ == '__main__':
+  useFullDataSet = (sys.argv[1].lower() in ['full', 'fulldata', 'fulldataset']) if (len(sys.argv) > 1) else False
   currDirectoryOfScript = os.path.dirname(os.path.realpath(__file__))
-  cleanDataLocation = '/'.join([currDirectoryOfScript, '..', 'Data', 'HotelData', 'testData']) # TODO: switch from testData to CleanData
-  resultsLocation = '/'.join([currDirectoryOfScript, '..', 'Results', 'HotelFinalResults.txt'])
+  cleanDataLocation = '/'.join([currDirectoryOfScript, '..', 'Data', 'HotelData', 'cleanData' if useFullDataSet else 'testData'])
+  resultsLocation = '/'.join([currDirectoryOfScript, '..', 'Results', 'FullSet_HotelFinalResults.txt' if useFullDataSet else 'TestSet_HotelFinalResults.txt'])
   stopWords = genStopwords()
   itemList, reviewDataList = getData(cleanDataLocation)
   print('DEBUG: getData')
